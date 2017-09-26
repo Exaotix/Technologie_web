70381cffbe502623cb547acd65f227c01aadbd87
Thiebaut Eric <e-riic@hotmail.com>
2017-09-26 16:00:19 +0200

    Ajout début texte html 1

---
+++ Labo2.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = "RESERVATION";
+
+$b = "Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros.";
+$c = "Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageurs.";
+
+$d = "Destination";
+
+
+echo $a."<br>"."<br>";
+
+echo $b."<br>"."<br>";
+
+echo $c."<br>";
+
+
+/*
+<form method="post" action="cible.php">
+ 
+<p>
+    <input type="text" />
+</p>
+ 
+</form>
+
+
+?>
+*/
